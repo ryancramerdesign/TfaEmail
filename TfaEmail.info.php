00bed95701dc084351cfbf5924fd96f987e01715
Ryan Cramer <ryan@processwire.com>
2020-06-12 11:44:09 -0400

    Update TfaEmail to support auto-enable in PW 3.0.160, add option for user to choose existing email (in separate email field) or custom defined email with module. Add support for selection of which WireMail module should be used in module configuration. Update sent email to show code first before other text. Remove email-to-SMS references in text, which might confuse some users that don't know about that option.

--- TfaEmail.info.php
+++ TfaEmail.info.php
@@ -1,8 +1,8 @@
 <?php namespace ProcessWire;
 $info = array(
-	'title' => 'Email/SMS two-factor authentication',
+	'title' => 'Email two-factor authentication',
 	'summary' => 'Performs two-factor authentication by sending user an email with a code. Also works with email-to-SMS.',
-	'version' => 1,
+	'version' => 2,
 	'singular' => true,
 	'requires' => 'ProcessWire>=3.0.109'
 );
